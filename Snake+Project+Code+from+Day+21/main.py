c83d34a90f3e5ae45e42ea5458852f297df08ec3
J-Bills <billsjamal@gmail.com>
2024-05-06 13:02:27 -0700

    adding highscore.txt and highscore tracking between games

--- Snake+Project+Code+from+Day+21/main.py
+++ Snake+Project+Code+from+Day+21/main.py
@@ -4,6 +4,7 @@ from food import Food
 from scoreboard import Scoreboard
 import time
 
+
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
@@ -34,16 +35,17 @@ while game_is_on:
 
     #Detect collision with wall.
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        game_is_on = False
-        scoreboard.game_over()
+        snake.reset()
+        scoreboard.reset()
 
     #Detect collision with tail.
     for segment in snake.segments:
         if segment == snake.head:
             pass
         elif snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()
+            snake.reset()
+            scoreboard.reset()
+           
 
 
 
